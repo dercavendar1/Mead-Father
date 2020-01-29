e37411549b30258d6d032b4f5709c9076577ee2d
dercavendar1 <52138485+dercavendar1@users.noreply.github.com>
2020-01-29 11:50:13 -0600

    Initial upload of base python search script

---
+++ mariadb.py
@@ -0,0 +1,36 @@
+import pymysql as db
+
+# Innitiates connection to DB
+connection = db.connect(host='192.168.1.210',
+                        user='root',
+                        password='#d8K*omZWK%J9b6P',
+                        database='homebrew_proj',
+                        charset='utf8mb4',
+                        cursorclass=db.cursors.DictCursor)
+# Grabs flavor table from DB and stores in flavors variable
+sql_query = ('select * from flavors')
+with connection.cursor() as cursor:
+    cursor.execute(sql_query)
+    flavors = cursor.fetchall()
+
+# Creates a query and converts the string to the corresponding
+# primary key in flavor table to simplify sql query
+query = input('What flavor are you looking for? ')
+for flavor in flavors:
+    if query == flavor['flavor_profile']:
+        query = flavor['flavor_id']
+
+sql_query = (f'select honey_name, flavors.flavor_profile from honey ' +
+             f'inner join flavors on honey.flavor_profile = flavor_id' +
+             f' where flavors.flavor_id = {query}')
+# queries DB based on input search string and stores results in a list of Dictionaries
+with connection.cursor() as cursor:
+    cursor.execute(sql_query)
+    results = cursor.fetchall()
+
+# prints results of search or no result message
+if len(results) == 0:
+    print('No Results Found')
+else:
+    for result in results:
+        print(result)
